f96eaa889356f2fe107f9fda5b4696daae30547f
IzakDev <141975061+IzakStudios@users.noreply.github.com>
2024-02-07 09:57:22 +0000

    a

--- js/main.js
+++ js/main.js
@@ -132,7 +132,7 @@ const loadQuiz = async () => {
     
     var Quiz = new QuizLib([
         {
-            "question": "Question 1",
+            "question": "Template Question",
             "content": "What is the opposite of 'Goodbye'?",
 
             "answer": "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824",
